5f3e309887a56b1d1153975d8578fcc86b9286d2
Jahfer Husain <jahfer.husain@shopify.com>
2021-03-05 08:54:14 -0500

    Changes

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+panic() {
+  printf "%s: %.0s${1}\n" "${0##*/}" "$@" >&2
+  exit 1
+}
 
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 EMACS_PATH = "~/.emacs.d"
-mkdir -p ${SPIN_EMACS_PATH}
-[ -L ${SPIN_EMACS_PATH}/init.el ] || ln -s ${DOTFILES}/emacs/init.el ${SPIN_EMACS_PATH}/init.el
-[ -L ${SPIN_EMACS_PATH}/scratch-template ] || ln -s ${DOTFILES}/emacs/scratch-template ${SPIN_EMACS_PATH}/scratch-template
+mkdir -p ${SPIN_EMACS_PATH} || panic "Failed to mkdir -p ${SPIN_EMACS_PATH}"
+[ -L ${SPIN_EMACS_PATH}/init.el ] || ln -s ${DOTFILES}/emacs/init.el ${SPIN_EMACS_PATH}/init.el || panic "Failed to link emacs init.el"
+[ -L ${SPIN_EMACS_PATH}/scratch-template ] || ln -s ${DOTFILES}/emacs/scratch-template ${SPIN_EMACS_PATH}/scratch-template || panic "Failed to link emacs scratch-template"
 
 # # vim
 # ln -s ${BASEDIR}/vimrc ~/.vimrc
