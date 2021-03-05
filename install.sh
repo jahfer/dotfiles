94aa064268324fbe0e149e3337d84af8ed46b4ba
Jahfer Husain <jahfer.husain@shopify.com>
2021-03-05 08:51:51 -0500

    Add quotes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-EMACS_PATH = ~/.emacs.d
+EMACS_PATH = "~/.emacs.d"
 mkdir -p ${SPIN_EMACS_PATH}
 [ -L ${SPIN_EMACS_PATH}/init.el ] || ln -s ${DOTFILES}/emacs/init.el ${SPIN_EMACS_PATH}/init.el
 [ -L ${SPIN_EMACS_PATH}/scratch-template ] || ln -s ${DOTFILES}/emacs/scratch-template ${SPIN_EMACS_PATH}/scratch-template
