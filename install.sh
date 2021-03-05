7f172902d0920547542bb8a7641bb17e45cb964a
Jahfer Husain <jahfer.husain@shopify.com>
2021-03-05 08:38:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# emacs
 mkdir -p ~/.emacs.d
-[ -f ~/.emacs.d/init.el ] ||  ln -s -f ${BASEDIR}/emacs/init.el
-
-# # vim
-# ln -s ${BASEDIR}/vimrc ~/.vimrc
-# ln -s ${BASEDIR}/vim/ ~/.vim
-
-# # zsh
-# ln -s ${BASEDIR}/zshrc ~/.zshrc
-
-# # git
-# ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+if [ ! -f ~/.emacs.d/init.el ]; then
+  ln -s ${BASEDIR}/emacs/init.el
+fi
