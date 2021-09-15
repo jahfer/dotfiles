0efeec33ff18a934b3aea832e2093cc62ffe248c
Jahfer Husain <jahfer.husain@shopify.com>
2021-09-15 09:57:55 -0400

    Last try zprezto

--- system/01_prezto.zsh
+++ system/01_prezto.zsh
@@ -1,3 +1,8 @@
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  [ ! -e "${ZDOTDIR:-$HOME}/.${rcfile:t}" ] && ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
 # Source Prezto.
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,3 @@
-PATH=$HOME/bin:$PATH
+PATH=$HOME/bin:${ZDOTDIR:-$HOME}/.zprofile:$PATH
 EDITOR=emacs
 FZF_DEFAULT_COMMAND='rg --files'
