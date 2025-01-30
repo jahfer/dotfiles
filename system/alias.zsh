66c4ebbf34a07e169c3481a8ccb91da1274a7dd6
Jahfer Husain <jahfer.husain@shopify.com>
2025-01-30 15:03:12 -0500

    Update alias.zsh

--- system/alias.zsh
+++ system/alias.zsh
@@ -10,5 +10,6 @@ alias tc="tmux -CC"
 alias ta="tmux -CC attach"
 
 # search
-alias f="fzf --preview 'batcat --color=always --style=numbers --line-range=:500 {}'"
+alias f="fzf --preview 'bat --color=always --theme=TwoDark --style=numbers --line-range=:500 {}'"
 alias lsn='nnn -de'
+alias fd="fdfind"
