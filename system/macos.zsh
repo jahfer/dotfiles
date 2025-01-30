757bfd0f1558470ac41a4bde917bb9e95dde53f6
Jahfer Husain <jahfer.husain@shopify.com>
2025-01-30 15:04:41 -0500

    Create macos.zsh

---
+++ system/macos.zsh
@@ -0,0 +1,4 @@
+if [ -f "${HOMEBREW_PREFIX}/opt/chruby/share/chruby/chruby.sh" ]; then
+  source "${HOMEBREW_PREFIX}/opt/chruby/share/chruby/chruby.sh"
+  source "${HOMEBREW_PREFIX}/opt/chruby/share/chruby/auto.sh"
+fi
