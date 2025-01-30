4ed2e8e3987d83db0d95bfb68b1ceb3f90eab8f0
Jahfer Husain <jahfer.husain@shopify.com>
2025-01-30 15:11:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,12 +28,9 @@ mkdir -p $SPIN_EMACS_PATH || panic "Failed to mkdir -p ${SPIN_EMACS_PATH}"
 
 # Install Tig
 if ! command -v tig &> /dev/null; then
-  [ dpkg -s libncurses5-dev ] || sudo apt-get -o DPkg::Lock::Timeout=60 install -y libncurses5-dev
-  [ dpkg -s libncursesw5-dev ] || sudo apt-get -o DPkg::Lock::Timeout=60 install -y libncursesw5-dev
-
   wget -O - "https://github.com/jonas/tig/releases/download/tig-${TIG_VERSION}/tig-${TIG_VERSION}.tar.gz" | tar -zxf - || panic "Failed to download tig"
 
-  if [ ! -d "/home/spin/bin/tig" ]; then
+  if [ ! -d "${HOME}/bin/tig" ]; then
     cd ./tig-${TIG_VERSION}
     make || panic "Failed to make tig"
     make install || panic "Failed to install tig"
@@ -41,11 +38,13 @@ if ! command -v tig &> /dev/null; then
 fi
 [ -L $HOME/.tigrc ] || ln -s ${DOTFILES}/tig/tigrc ${HOME}/.tigrc || panic "Failed to symlink .tigrc"
 
+
+cd "${DOTFILES}" || panic "Failed to cd into dotfiles directory"
+
 # Install nnn
 if ! command -v nnn &> /dev/null; then
-  sudo apt-get -o DPkg::Lock::Timeout=60 install -y pkg-config libncursesw5-dev libreadline-dev || panic "Failed to install dependencies for nnn"
   wget -O - "https://github.com/jarun/nnn/releases/download/v${NNN_VERSION}/nnn-v${NNN_VERSION}.tar.gz" | tar -zxf - || panic "Failed to download nnn"
-  cd ./nnn-${NNN_VERSION}
+  cd ./nnn-${NNN_VERSION} || panic "Failed to cd into nnn directory"
   sudo make strip install || panic "Failed to install nnn"
 
   curl -Ls "https://raw.githubusercontent.com/jarun/nnn/v${NNN_VERSION}/plugins/getplugs" | sh || panic "Failed to install nnn plugins"
@@ -58,24 +57,24 @@ fi
 
 # Install fd
 if ! command -v fdfind &> /dev/null; then
-  sudo apt-get -o DPkg::Lock::Timeout=60 install -y fd-find
-  sudo ln -s $(which fdfind) "${HOME}/bin/fd" || panic "Failed to symlind fdfind"
+  # sudo apt-get -o DPkg::Lock::Timeout=60 install -y fd-find
+  # sudo ln -s $(which fdfind) "${HOME}/bin/fd" || panic "Failed to symlind fdfind"
 fi
 
 # Install bat
 if ! command -v batcat &> /dev/null; then
-  sudo apt-get -o DPkg::Lock::Timeout=60 -o Dpkg::Options::="--force-overwrite" install -y bat
+  # sudo apt-get -o DPkg::Lock::Timeout=60 -o Dpkg::Options::="--force-overwrite" install -y bat
 fi
 
 # Install ripgrep
 if ! command -v rg &> /dev/null; then
-  sudo apt-get -o DPkg::Lock::Timeout=60 install ripgrep
+  # sudo apt-get -o DPkg::Lock::Timeout=60 install ripgrep
 fi
 
 # Install delta
 if ! command -v delta &> /dev/null; then
-  wget "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-delta_${DELTA_VERSION}_amd64.deb" || panic "Failed to download git-delta"
-  sudo dpkg -i git-delta_${DELTA_VERSION}_amd64.deb
+  # wget "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-delta_${DELTA_VERSION}_amd64.deb" || panic "Failed to download git-delta"
+  # sudo dpkg -i git-delta_${DELTA_VERSION}_amd64.deb
 fi
 
 # Config Files
@@ -85,3 +84,14 @@ fi
 
 # git
 rm -f ${HOME}/.gitconfig && ln -s ${DOTFILES}/git/gitconfig ${HOME}/.gitconfig || panic "Failed to symlink .gitconfig"
+
+
+# Symbolic link of fdfind -> fd
+if ! command -v fd &> /dev/null; then
+  ln -s "$(which fdfind)" "${HOME}/bin/fd" || panic "Failed to symlink fdfind as fd"
+fi
+
+# batcat
+if command -v batcat &> /dev/null; then
+  ln -s "$(which batcat)" "${HOME}/bin/bat" || panic "Failed to symlink batcat as bat"
+fi
